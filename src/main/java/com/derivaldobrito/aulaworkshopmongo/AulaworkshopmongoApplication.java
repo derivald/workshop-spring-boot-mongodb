9b220b854103b09ef8ca5056a892d3fdf8f4250d
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-08-25 00:04:41 -0300

    Primeiro comit - projeto criado

---
+++ src/main/java/com/derivaldobrito/aulaworkshopmongo/AulaworkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.derivaldobrito.aulaworkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaworkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaworkshopmongoApplication.class, args);
+	}
+
+}
